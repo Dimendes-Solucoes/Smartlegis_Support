259d9cfc60a4227ad9fc6c36826f2d37919b01ef
Daniel Arruda <arruda.daniel20@gmail.com>
2025-08-27 11:34:07 -0300

    refactor: simplificar a estrutura do layout da página Clicksign

--- resources/js/Pages/Clicksign/Index.vue
+++ resources/js/Pages/Clicksign/Index.vue
@@ -46,43 +46,36 @@ const closeModal = () => {
     <Head title="Clicksign" />
 
     <AuthenticatedLayout>
-        <div class="py-12">
-            <div class="mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900 dark:text-gray-100">
-                        <div v-if="clicksigns.length > 0" class="overflow-x-auto">
-                            <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700 text-sm">
-                                <thead class="bg-gray-50 dark:bg-gray-700">
-                                    <tr>
-                                        <th scope="col"
-                                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider dark:text-gray-300">
-                                            Cidade</th>
-                                        <th scope="col"
-                                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider dark:text-gray-300">
-                                            Quantidade</th>
-                                        <th></th>
-                                    </tr>
-                                </thead>
-                                <tbody class="bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700">
-                                    <tr v-for="clicksign in clicksigns" :key="clicksign.tenant_id">
-                                        <td class="px-6 py-4 whitespace-nowrap">{{ clicksign.tenant_city }}</td>
-                                        <td class="px-6 py-4 whitespace-nowrap">{{ clicksign.quantity }}</td>
-                                        <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                            <IconButton @click="openConfirmDeleteModal(clicksign)" color="red"
-                                                title="Limpar" class="ml-2">
-                                                <TrashIcon class="h-5 w-5" />
-                                            </IconButton>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                        <div v-else>
-                            <p>Nenhum evento clicksign encontrado.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+
+        <div v-if="clicksigns.length > 0" class="overflow-x-auto">
+            <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700 text-sm">
+                <thead class="bg-gray-50 dark:bg-gray-700">
+                    <tr>
+                        <th scope="col"
+                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider dark:text-gray-300">
+                            Cidade</th>
+                        <th scope="col"
+                            class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider dark:text-gray-300">
+                            Quantidade</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody class="bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700">
+                    <tr v-for="clicksign in clicksigns" :key="clicksign.tenant_id">
+                        <td class="px-6 py-4 whitespace-nowrap">{{ clicksign.tenant_city }}</td>
+                        <td class="px-6 py-4 whitespace-nowrap">{{ clicksign.quantity }}</td>
+                        <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                            <IconButton @click="openConfirmDeleteModal(clicksign)" color="red" title="Limpar"
+                                class="ml-2">
+                                <TrashIcon class="h-5 w-5" />
+                            </IconButton>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div v-else>
+            <p>Nenhum evento clicksign encontrado.</p>
         </div>
     </AuthenticatedLayout>
 
