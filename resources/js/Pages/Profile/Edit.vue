de7c9abb19f04806f9ea17d68f95f82e7c5145a8
Daniel Arruda <arruda.daniel20@gmail.com>
2025-08-27 14:47:24 -0300

    refactor: simplificar a estrutura do componente de edição de perfil para melhorar a legibilidade

--- resources/js/Pages/Profile/Edit.vue
+++ resources/js/Pages/Profile/Edit.vue
@@ -11,34 +11,17 @@ defineProps<{
 </script>
 
 <template>
+
     <Head title="Profile" />
 
     <AuthenticatedLayout>
-        <template #header>
-            <h2
-                class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200"
-            >
-                Profile
-            </h2>
-        </template>
+        <div class="mx-auto space-y-4">
+            <div class="bg-white lg:p-4 p-2 border sm:rounded-lg dark:bg-gray-800">
+                <UpdateProfileInformationForm :must-verify-email="mustVerifyEmail" :status="status" class="max-w-xl" />
+            </div>
 
-        <div class="py-12">
-            <div class="mx-auto space-y-6 sm:px-6 lg:px-8">
-                <div
-                    class="bg-white p-4 shadow sm:rounded-lg sm:p-8 dark:bg-gray-800"
-                >
-                    <UpdateProfileInformationForm
-                        :must-verify-email="mustVerifyEmail"
-                        :status="status"
-                        class="max-w-xl"
-                    />
-                </div>
-
-                <div
-                    class="bg-white p-4 shadow sm:rounded-lg sm:p-8 dark:bg-gray-800"
-                >
-                    <UpdatePasswordForm class="max-w-xl" />
-                </div>
+            <div class="bg-white lg:p-4 p-2 border sm:rounded-lg dark:bg-gray-800">
+                <UpdatePasswordForm class="max-w-xl" />
             </div>
         </div>
     </AuthenticatedLayout>
