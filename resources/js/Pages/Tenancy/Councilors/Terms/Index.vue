5357d05dca405b4be2cdc8725fe0edcaa1a35cc9
Daniel Arruda <arruda.daniel20@gmail.com>
2025-10-14 17:15:05 -0300

    fix: corrigindo espacos na formatacao da index de mandatos do vereador

--- resources/js/Pages/Tenancy/Councilors/Terms/Index.vue
+++ resources/js/Pages/Tenancy/Councilors/Terms/Index.vue
@@ -28,7 +28,6 @@ const formatDate = (dateString: string | null): string => {
 const form = useForm({
     id: null as number | null,
     start_date: '',
-    // 1. CORRIGIDO: O tipo de 'end_date' agora pode ser nulo
     end_date: null as string | null,
 });
 
@@ -42,7 +41,6 @@ const openCreateModal = () => {
 const openEditModal = (term: Term) => {
     form.id = term.id;
     form.start_date = term.start_date;
-    // 2. CORRIGIDO: Garante que 'end_date' seja string ou nulo
     form.end_date = term.end_date || null;
     showTermModal.value = true;
 };
@@ -53,11 +51,11 @@ const closeModal = () => {
 };
 
 const submitTerm = () => {
-    if (form.id) { 
+    if (form.id) {
         form.put(route('councilors.terms.update', form.id), {
             onSuccess: () => closeModal(),
         });
-    } else { 
+    } else {
         form.post(route('councilors.terms.store', props.data.councilor.id), {
             onSuccess: () => closeModal(),
         });
@@ -84,51 +82,59 @@ const deleteTerm = () => {
 </script>
 
 <template>
+
     <Head :title="`Mandatos de ${data.councilor.name}`" />
 
     <AuthenticatedLayout>
-                <BackButtonRow :href="route('councilors.index')" />
+        <BackButtonRow :href="route('councilors.index')" />
 
-                <div class="flex justify-between items-center mb-6 mt-4">
-                    <h1 class="text-2xl font-bold text-gray-800 dark:text-gray-200">
-                        Gerenciar Mandatos de: {{ data.councilor.name }}
-                    </h1>
-                    <PrimaryButton @click="openCreateModal" class="flex items-center">
-                        <PlusIcon class="h-5 w-5 mr-2" />
-                        Adicionar Mandato
-                    </PrimaryButton>
-                </div>
+        <div class="flex justify-between items-center mb-6 mt-4">
+            <h1 class="text-xl font-bold text-gray-800 dark:text-gray-200">
+                Gerenciar Mandatos de: {{ data.councilor.name }}
+            </h1>
+            <PrimaryButton @click="openCreateModal" class="flex items-center">
+                <PlusIcon class="h-5 w-5 mr-2" />
+                Adicionar Mandato
+            </PrimaryButton>
+        </div>
 
-                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                    <div v-if="data.terms.length > 0">
-                        <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
-                            <thead class="bg-gray-50 dark:bg-gray-700/50">
-                                <tr>
-                                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">Data de Início</th>
-                                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">Data de Fim</th>
-                                    <th class="relative px-6 py-3"></th>
-                                </tr>
-                            </thead>
-                            <tbody class="divide-y divide-gray-200 dark:divide-gray-700">
-                                <tr v-for="term in data.terms" :key="term.id">
-                                    <td class="px-6 py-4">{{ formatDate(term.start_date) }}</td>
-                                    <td class="px-6 py-4">{{ formatDate(term.end_date) }}</td>
-                                    <td class="px-6 py-4 text-right">
-                                        <div class="flex items-center justify-end space-x-1">
-                                            <IconButton @click="openEditModal(term)" color="yellow" title="Editar"><PencilSquareIcon class="h-5 w-5" /></IconButton>
-                                            <IconButton @click="openConfirmDeleteModal(term)" color="red" title="Excluir"><TrashIcon class="h-5 w-5" /></IconButton>
-                                        </div>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                    <div v-else class="p-6 text-center text-gray-500">
-                        <p>Nenhum mandato cadastrado para este vereador.</p>
-                    </div>
-                </div>
+        <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+            <div v-if="data.terms.length > 0">
+                <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
+                    <thead class="bg-gray-50 dark:bg-gray-700/50">
+                        <tr>
+                            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">Data de Início
+                            </th>
+                            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">Data de Fim</th>
+                            <th class="relative px-6 py-3"></th>
+                        </tr>
+                    </thead>
+                    <tbody class="divide-y divide-gray-200 dark:divide-gray-700">
+                        <tr v-for="term in data.terms" :key="term.id">
+                            <td class="px-6 py-4">{{ formatDate(term.start_date) }}</td>
+                            <td class="px-6 py-4">{{ formatDate(term.end_date) }}</td>
+                            <td class="px-6 py-4 text-right">
+                                <div class="flex items-center justify-end space-x-1">
+                                    <IconButton @click="openEditModal(term)" color="yellow" title="Editar">
+                                        <PencilSquareIcon class="h-5 w-5" />
+                                    </IconButton>
+                                    <IconButton @click="openConfirmDeleteModal(term)" color="red" title="Excluir">
+                                        <TrashIcon class="h-5 w-5" />
+                                    </IconButton>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div v-else class="p-6 text-center text-gray-500">
+                <p>Nenhum mandato cadastrado para este vereador.</p>
+            </div>
+        </div>
     </AuthenticatedLayout>
-    
+
     <TermModal :show="showTermModal" :form="form" @close="closeModal" @submit="submitTerm" />
-    <ConfirmDeletionModal :show="confirmingDeletion" title="Excluir Mandato" message="Tem certeza que deseja excluir este mandato?" buttonText="Excluir" @close="closeDeleteModal" @confirm="deleteTerm" />
+    <ConfirmDeletionModal :show="confirmingDeletion" title="Excluir Mandato"
+        message="Tem certeza que deseja excluir este mandato?" buttonText="Excluir" @close="closeDeleteModal"
+        @confirm="deleteTerm" />
 </template>
