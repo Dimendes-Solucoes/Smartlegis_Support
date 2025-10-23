a8d274209b8929a8e382b2cc2690a126019cb290
Daniel Arruda <arruda.daniel20@gmail.com>
2025-10-23 16:11:24 -0300

    feat: adicionar componente RegularColumn e integrá-lo nas páginas de credenciais e sessões

--- resources/js/Pages/Tenancy/Sessions/Index.vue
+++ resources/js/Pages/Tenancy/Sessions/Index.vue
@@ -1,12 +1,21 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import SessionStatusBadge from '@/Components/Session/SessionStatusBadge.vue';
-import { Head, router, Link } from '@inertiajs/vue3';
-import { TrashIcon, ChevronUpIcon, ChevronDownIcon, PencilSquareIcon, DocumentDuplicateIcon, ArrowPathIcon, ListBulletIcon } from '@heroicons/vue/24/outline';
-import IconButton from '@/Components/Itens/IconButton.vue';
-import ConfirmDeletionModal from '@/Components/ConfirmDeletionModal.vue';
-import TextButton from '@/Components/Itens/TextButton.vue';
+import { ref } from "vue";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import SessionStatusBadge from "@/Components/Session/SessionStatusBadge.vue";
+import { Head, router, Link } from "@inertiajs/vue3";
+import IconButton from "@/Components/Itens/IconButton.vue";
+import ConfirmDeletionModal from "@/Components/ConfirmDeletionModal.vue";
+import TextButton from "@/Components/Itens/TextButton.vue";
+import RegularColumn from "@/Components/Table/RegularColumn.vue";
+import {
+    TrashIcon,
+    ChevronUpIcon,
+    ChevronDownIcon,
+    PencilSquareIcon,
+    DocumentDuplicateIcon,
+    ArrowPathIcon,
+    ListBulletIcon,
+} from "@heroicons/vue/24/outline";
 
 interface Session {
     id: number;
@@ -29,29 +38,35 @@ const props = defineProps<{
     filters: {
         sort: string;
         direction: string;
-    }
+    };
 }>();
 
 const sortBy = (field: string) => {
-    let direction = 'asc';
-    if (props.filters.sort === field && props.filters.direction === 'asc') {
-        direction = 'desc';
+    let direction = "asc";
+    if (props.filters.sort === field && props.filters.direction === "asc") {
+        direction = "desc";
     }
 
-    router.get(route('sessions.index'), {
-        sort: field,
-        direction: direction,
-    }, {
-        preserveState: true,
-        replace: true,
-    });
+    router.get(
+        route("sessions.index"),
+        {
+            sort: field,
+            direction: direction,
+        },
+        {
+            preserveState: true,
+            replace: true,
+        }
+    );
 };
 
 const formatDate = (datetime: string) => {
-    if (!datetime) return 'Não definida';
+    if (!datetime) return "Não definida";
     const date = new Date(datetime);
-    return date.toLocaleString('pt-BR', {
-        day: '2-digit', month: '2-digit', year: 'numeric',
+    return date.toLocaleString("pt-BR", {
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric",
     });
 };
 
@@ -66,7 +81,7 @@ const openConfirmDeleteModal = (session: Session) => {
 const deleteSession = () => {
     if (!sessionToDelete.value) return;
 
-    router.delete(route('sessions.destroy', sessionToDelete.value.id), {
+    router.delete(route("sessions.destroy", sessionToDelete.value.id), {
         preserveScroll: true,
         onSuccess: () => closeModal(),
     });
@@ -82,10 +97,14 @@ const openConfirmResetModal = (session: Session) => {
 
 const resetSession = () => {
     if (!sessionToReset.value) return;
-    router.post(route('sessions.reset', sessionToReset.value.id), {}, {
-        preserveScroll: true,
-        onSuccess: () => closeModal(),
-    });
+    router.post(
+        route("sessions.reset", sessionToReset.value.id),
+        {},
+        {
+            preserveScroll: true,
+            onSuccess: () => closeModal(),
+        }
+    );
 };
 
 const confirmingSessionDuplication = ref(false);
@@ -98,10 +117,14 @@ const openConfirmDuplicateModal = (session: Session) => {
 
 const duplicateSession = () => {
     if (!sessionToDuplicate.value) return;
-    router.post(route('sessions.duplicate', sessionToDuplicate.value.id), {}, {
-        preserveScroll: true,
-        onSuccess: () => closeModal(),
-    });
+    router.post(
+        route("sessions.duplicate", sessionToDuplicate.value.id),
+        {},
+        {
+            preserveScroll: true,
+            onSuccess: () => closeModal(),
+        }
+    );
 };
 
 const closeModal = () => {
@@ -115,86 +138,140 @@ const closeModal = () => {
 </script>
 
 <template>
-
     <Head title="Sessões" />
 
     <AuthenticatedLayout>
         <div class="flex justify-end items-center mb-4">
-            <TextButton :href="route('sessions.create')">
-                Nova Sessão
-            </TextButton>
+            <TextButton :href="route('sessions.create')"> Nova Sessão </TextButton>
         </div>
 
         <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
             <div class="overflow-x-auto">
-                <table v-if="props.sessions.data.length > 0"
-                    class="min-w-full divide-y divide-gray-200 dark:divide-gray-700 text-sm">
+                <table
+                    v-if="props.sessions.data.length > 0"
+                    class="min-w-full divide-y divide-gray-200 dark:divide-gray-700 text-sm"
+                >
                     <thead class="bg-gray-50 dark:bg-gray-700/50">
                         <tr>
-                            <th scope="col"
-                                class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider dark:text-gray-400">
-                                <button @click="sortBy('name')" class="flex items-center space-x-1 uppercase">
+                            <th
+                                scope="col"
+                                class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider dark:text-gray-400"
+                            ></th>
+                            <RegularColumn>
+                                <button
+                                    @click="sortBy('name')"
+                                    class="flex items-center space-x-1 uppercase"
+                                >
                                     <span>Nome da Sessão</span>
-                                    <ChevronUpIcon v-if="filters.sort === 'name' && filters.direction === 'asc'"
-                                        class="h-4 w-4" />
-                                    <ChevronDownIcon v-if="filters.sort === 'name' && filters.direction === 'desc'"
-                                        class="h-4 w-4" />
+                                    <ChevronUpIcon
+                                        v-if="
+                                            filters.sort === 'name' &&
+                                            filters.direction === 'asc'
+                                        "
+                                        class="h-4 w-4"
+                                    />
+                                    <ChevronDownIcon
+                                        v-if="
+                                            filters.sort === 'name' &&
+                                            filters.direction === 'desc'
+                                        "
+                                        class="h-4 w-4"
+                                    />
                                 </button>
-                            </th>
-                            <th scope="col"
-                                class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider dark:text-gray-400">
-                                <button @click="sortBy('datetime_start')" class="flex items-center space-x-1 uppercase">
+                            </RegularColumn>
+
+                            <RegularColumn>
+                                <button
+                                    @click="sortBy('datetime_start')"
+                                    class="flex items-center space-x-1 uppercase"
+                                >
                                     <span>Data de Início</span>
                                     <ChevronUpIcon
-                                        v-if="filters.sort === 'datetime_start' && filters.direction === 'asc'"
-                                        class="h-4 w-4" />
+                                        v-if="
+                                            filters.sort === 'datetime_start' &&
+                                            filters.direction === 'asc'
+                                        "
+                                        class="h-4 w-4"
+                                    />
                                     <ChevronDownIcon
-                                        v-if="filters.sort === 'datetime_start' && filters.direction === 'desc'"
-                                        class="h-4 w-4" />
+                                        v-if="
+                                            filters.sort === 'datetime_start' &&
+                                            filters.direction === 'desc'
+                                        "
+                                        class="h-4 w-4"
+                                    />
                                 </button>
-                            </th>
-                            <th scope="col"
-                                class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider dark:text-gray-400">
-                                Status
-                            </th>
-                            <th scope="col"
-                                class="relative px-6 py-3 text-right text-xs font-medium text-gray-500 uppercase tracking-wider dark:text-gray-400">
+                            </RegularColumn>
+                            <RegularColumn>Status</RegularColumn>
+                            <th
+                                scope="col"
+                                class="relative px-6 py-3 text-right text-xs font-medium text-gray-500 uppercase tracking-wider dark:text-gray-400"
+                            >
                                 Ações
                             </th>
                         </tr>
                     </thead>
-                    <tbody class="bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700">
-                        <tr v-for="session in props.sessions.data" :key="session.id"
-                            class="hover:bg-gray-50 dark:hover:bg-gray-700/50">
-                            <td class="px-6 py-4 whitespace-normal">{{ session.name }}</td>
-                            <td class="px-6 py-4 whitespace-nowrap">{{ formatDate(session.datetime_start) }}</td>
+                    <tbody
+                        class="bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700"
+                    >
+                        <tr
+                            v-for="session in props.sessions.data"
+                            :key="session.id"
+                            class="hover:bg-gray-50 dark:hover:bg-gray-700/50"
+                        >
+                            <td class="px-6 py-4 whitespace-normal">
+                                {{ session.name }}
+                            </td>
+                            <td class="px-6 py-4 whitespace-nowrap">
+                                {{ formatDate(session.datetime_start) }}
+                            </td>
                             <td class="px-6 py-4 whitespace-nowrap">
                                 <SessionStatusBadge :status="session.session_status_id" />
                             </td>
-                            <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                            <td
+                                class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium"
+                            >
                                 <div class="flex items-center justify-end space-x-1">
-                                    <IconButton as="button" color="purple" title="Duplicar Sessão"
-                                        @click.stop="openConfirmDuplicateModal(session)">
+                                    <IconButton
+                                        as="button"
+                                        color="purple"
+                                        title="Duplicar Sessão"
+                                        @click.stop="openConfirmDuplicateModal(session)"
+                                    >
                                         <DocumentDuplicateIcon class="h-5 w-5" />
                                     </IconButton>
 
-                                    <IconButton as="button" color="blue" title="Resetar Sessão"
-                                        @click.stop="openConfirmResetModal(session)">
+                                    <IconButton
+                                        as="button"
+                                        color="blue"
+                                        title="Resetar Sessão"
+                                        @click.stop="openConfirmResetModal(session)"
+                                    >
                                         <ArrowPathIcon class="h-5 w-5" />
                                     </IconButton>
 
-                                    <IconButton :href="route('sessions.documents', session.id)" color="green"
-                                        title="Documentos da Sessão">
+                                    <IconButton
+                                        :href="route('sessions.documents', session.id)"
+                                        color="green"
+                                        title="Documentos da Sessão"
+                                    >
                                         <ListBulletIcon class="h-5 w-5" />
                                     </IconButton>
 
-                                    <IconButton :href="route('sessions.edit', session.id)" color="yellow"
-                                        title="Editar Sesssão">
+                                    <IconButton
+                                        :href="route('sessions.edit', session.id)"
+                                        color="yellow"
+                                        title="Editar Sesssão"
+                                    >
                                         <PencilSquareIcon class="h-5 w-5" />
                                     </IconButton>
 
-                                    <IconButton as="button" color="red" title="Excluir Sessão"
-                                        @click.stop="openConfirmDeleteModal(session)">
+                                    <IconButton
+                                        as="button"
+                                        color="red"
+                                        title="Excluir Sessão"
+                                        @click.stop="openConfirmDeleteModal(session)"
+                                    >
                                         <TrashIcon class="h-5 w-5" />
                                     </IconButton>
                                 </div>
@@ -204,31 +281,57 @@ const closeModal = () => {
                 </table>
             </div>
 
-            <div v-if="props.sessions.data.length === 0"
-                class="bg-white dark:bg-gray-800 rounded-lg p-6 text-center text-gray-500">
+            <div
+                v-if="props.sessions.data.length === 0"
+                class="bg-white dark:bg-gray-800 rounded-lg p-6 text-center text-gray-500"
+            >
                 <p>Nenhuma sessão encontrada.</p>
             </div>
         </div>
 
-        <div v-if="props.sessions.data.length > 0 && props.sessions.links.length > 3" class="mt-6 flex justify-center">
-            <Link v-for="(link, index) in props.sessions.links" :key="index" :href="link.url || ''"
-                class="px-4 py-2 text-sm" :class="{
+        <div
+            v-if="props.sessions.data.length > 0 && props.sessions.links.length > 3"
+            class="mt-6 flex justify-center"
+        >
+            <Link
+                v-for="(link, index) in props.sessions.links"
+                :key="index"
+                :href="link.url || ''"
+                class="px-4 py-2 text-sm"
+                :class="{
                     'bg-indigo-500 text-white rounded-md': link.active,
                     'text-gray-500 hover:text-gray-800': !link.active,
-                    'cursor-not-allowed text-gray-400': !link.url
-                }" v-html="link.label" />
+                    'cursor-not-allowed text-gray-400': !link.url,
+                }"
+                v-html="link.label"
+            />
         </div>
     </AuthenticatedLayout>
 
-    <ConfirmDeletionModal :show="confirmingSessionDeletion" title="Excluir Sessão"
+    <ConfirmDeletionModal
+        :show="confirmingSessionDeletion"
+        title="Excluir Sessão"
         :message="`Tem certeza que deseja mover a sessão '${sessionToDelete?.name}' para a lixeira?`"
-        :buttonText="'Excluir'" @close="closeModal" @confirm="deleteSession" />
+        :buttonText="'Excluir'"
+        @close="closeModal"
+        @confirm="deleteSession"
+    />
 
-    <ConfirmDeletionModal :show="confirmingSessionReset" title="Resetar Sessão"
+    <ConfirmDeletionModal
+        :show="confirmingSessionReset"
+        title="Resetar Sessão"
         :message="`Tem certeza que deseja restaurar a sessão '${sessionToReset?.name}' para o estado original? <strong>ESTA AÇÃO LIMPARÁ QUÓRUNS, VOTOS E FALAS; E NÃO PODERÁ SER DESFEITA</strong>`"
-        :buttonText="'Confirmar'" @close="closeModal" @confirm="resetSession" />
+        :buttonText="'Confirmar'"
+        @close="closeModal"
+        @confirm="resetSession"
+    />
 
-    <ConfirmDeletionModal :show="confirmingSessionDuplication" title="Duplicar Sessão"
+    <ConfirmDeletionModal
+        :show="confirmingSessionDuplication"
+        title="Duplicar Sessão"
         :message="`Tem certeza que deseja criar uma réplica da sessão '${sessionToDuplicate?.name}' e de toda a sua pauta?`"
-        :buttonText="'Confirmar'" @close="closeModal" @confirm="duplicateSession" />
+        :buttonText="'Confirmar'"
+        @close="closeModal"
+        @confirm="duplicateSession"
+    />
 </template>
